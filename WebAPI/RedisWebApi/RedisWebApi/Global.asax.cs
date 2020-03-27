edbb35eec4beac29282467de8b13d62346427aec
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-03-27 09:46:20 +0100

    Add fluent migrations

--- WebAPI/RedisWebApi/RedisWebApi/Global.asax.cs
+++ WebAPI/RedisWebApi/RedisWebApi/Global.asax.cs
@@ -6,6 +6,9 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using test;
 
 namespace RedisWebApi
 {
@@ -16,6 +19,48 @@ namespace RedisWebApi
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer2016()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Server=DESKTOP-NTNHKPL;Database=RedisApp;Trusted_Connection=True;")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(InitMigration).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
         }
     }
 }
